850fa7340fd43793fe9779c0c45c15291a43dfea
Dzung Doan <doanxuandung@yahoo.com>
2017-07-02 19:29:17 -0700

    Added parsePackageDependencies() and tests

--- src/main/scala/com/pluralsight/exercise/packageinstaller/Parser/InputParser.scala
+++ src/main/scala/com/pluralsight/exercise/packageinstaller/Parser/InputParser.scala
@@ -6,4 +6,28 @@ object InputParser {
   // Allows one package to have multiple dependencies, as a comma separated list
   // So "a: b" and "a: b, c" are both valid
   private[Parser] val DependenciesListRegEx = "\\s*,\\s*"
+
+  private[Parser] def parsePackageDependencies(packageAndDependencies: String) = {
+    val splitted = packageAndDependencies.split(PackageAndDependenciesRegEx)
+
+    val packageName = splitted(0).trim
+    if (packageName == "") {
+      throw new IllegalArgumentException("Invalid input")
+    }
+
+    // No dependencies
+    if (splitted.length == 1) {
+      List(("", packageName))
+    } else {
+      val dependenciesList = splitted(1).split(DependenciesListRegEx)
+
+      // Still no dependencies
+      if (dependenciesList.length == 0) {
+        List(("", packageName))
+      } else {
+        for(dependency <- dependenciesList) yield (dependency, packageName)
+      }
+
+    }
+  }
 }
--- src/test/scala/com/pluralsight/exercise/packageinstaller/Parser/TestInputParser.scala
+++ src/test/scala/com/pluralsight/exercise/packageinstaller/Parser/TestInputParser.scala
@@ -9,28 +9,28 @@ class TestInputParser extends WordSpec {
     "standard specification of package name" should {
       "succeed" in {
         val splitted = "a: b".split(PackageAndDependenciesRegEx)
-        assert(splitted(0) == "a")
+        assert(splitted(0) === "a")
       }
     }
 
     "there are white spaces after package name" should {
       "succeed" in {
         val splitted = "a   : b".split(PackageAndDependenciesRegEx)
-        assert(splitted(0) == "a")
+        assert(splitted(0) === "a")
       }
     }
 
     "there is no package name" should {
       "reflect that fact" in {
         val splitted = "  : b".split(PackageAndDependenciesRegEx)
-        assert(splitted(0) == "")
+        assert(splitted(0) === "")
       }
     }
 
     "there is no white space preceding the dependencies list" should {
       "fail to parse" in {
         val splitted = "a:b".split(PackageAndDependenciesRegEx)
-        assert(splitted.length == 1)
+        assert(splitted.length === 1)
       }
     }
   }
@@ -39,31 +39,59 @@ class TestInputParser extends WordSpec {
     "there is one dependency" should {
       "succeed" in {
         val splitted = "a".split(DependenciesListRegEx)
-        assert(splitted(0) == "a")
+        assert(splitted(0) === "a")
       }
     }
 
     "there are multiple dependencies" should {
       "succeed" in {
         val splitted = "a, b".split(DependenciesListRegEx)
-        assert(splitted(0) == "a")
-        assert(splitted(1) == "b")
+        assert(splitted(0) === "a")
+        assert(splitted(1) === "b")
       }
     }
 
     "there are extra white spaces and commas" should {
       "succeed" in {
         val splitted = "a , b  ,, , ".split(DependenciesListRegEx)
-        assert(splitted.length == 2)
-        assert(splitted(0) == "a")
-        assert(splitted(1) == "b")
+        assert(splitted.length === 2)
+        assert(splitted(0) === "a")
+        assert(splitted(1) === "b")
       }
     }
 
     "the list is actually empty" should {
       "succeed" in {
         val splitted = ",   ,, , ".split(DependenciesListRegEx)
-        assert(splitted.length == 0)
+        assert(splitted.length === 0)
+      }
+    }
+  }
+
+  "parsePackageDependencies()" when {
+    "there is no package name" should {
+      "throw an exception" in {
+        assertThrows[IllegalArgumentException] {
+          parsePackageDependencies("  : b")
+        }
+      }
+    }
+
+    "there is no dependency" should {
+      "return a single element list" in {
+        assert(parsePackageDependencies("a: ") === List(("", "a")))
+      }
+    }
+
+    "there is a single dependency" should {
+      "return a single element list" in {
+        assert(parsePackageDependencies("a: b") === List(("b", "a")))
+      }
+    }
+
+    "there are multiple dependencies" should {
+      "return multiple elements list" in {
+        assert(parsePackageDependencies("a: b, c") === List(("b", "a"), ("c", "a")))
       }
     }
   }
